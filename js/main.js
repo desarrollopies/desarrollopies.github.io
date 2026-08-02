ccd6b662df6cc101d2fee02682727b365968db68
desarrollopies <comunicandocf8@gmail.com>
2026-08-02 03:04:21 -0300

    Implement navbar scroll effect
    Add scroll event listener to toggle navbar class

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-// JavaScript de Asociación PIES
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 30) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+
+});
